27008a770c753b39ac9e5823695b37d0ccb8534a
tamarahadzi <tamarrikos@gmail.com>
2020-01-11 12:41:48 +0100

    web configuration

---
+++ src/main/java/com/example/spring/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.spring.service;
+
+import com.example.spring.model.CustomUserDetails;
+import com.example.spring.model.User;
+import com.example.spring.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByEmail(username);
+        optionalUser.orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return optionalUser.map(CustomUserDetails::new).get();
+    }
+
+}
